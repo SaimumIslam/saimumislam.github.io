cb2aa16cbacab76b10f645cfb8dd59a47c264b43
SaimumIslam <saimumislam27@gmail.com>
2025-12-10 01:35:41 +0600

    experience data updated

--- src/config/experiences.ts
+++ src/config/experiences.ts
@@ -1,28 +1,50 @@
-const EXPERIENCES = [
+const experiences = [
   {
-    title: "Web developer intern",
-    company: "WEB-BOX, Tarnobrzeg PL",
-    description:
-      "During this internship, I focused on two key areas: creating Wordpress plugins and managing website content, plugins, and settings.",
-    period: "2023",
-    technologies: ["HTML", "CSS", "JavaScript", "Wordpress"],
+    institute: "EDGE-KUCSE Digital Skills Training",
+    position: "Industrial Expert",
+    time: "April 2025 - June 2025",
+    points: [
+      "Conducted hands-on training on Python Programming and Basic Data Science for three participant batches, emphasizing real-world problem-solving, data handling, and predictive modeling techniques.",
+      "Designed and delivered workshop-based sessions integrating theory, presentations, and project-based learning.",
+      "Guided participants with little to no programming background through practical coding exercises, data science fundamentals, and collaborative project work.",
+    ],
   },
   {
-    title: "Frontend developer & UI designer",
-    company: "Freelancing, remote",
-    description:
-      "As a freelancer, I specialized in graphic design using Figma and website development using Next.js with Sanity CMS. I undertook projects independently, from design conceptualization to final implementation, ensuring client satisfaction.",
-    period: "2023 - 2024",
-    technologies: ["Next.js", "TypeScript", "Sanity CMS", "Tailwind"],
+    institute: "Jukto Digital",
+    position: "Senior Software Engineer",
+    time: "September 2021 - Present",
+    points: [
+      "Took ownership of core system design decisions and oversaw the full-stack development of a comprehensive client software upgrade, ensuring scalable architecture, efficient issue resolution, and enhanced overall system performance.",
+      "Engineered and delivered an automated form generation system within the Learning Management System to streamline the admission process, significantly reducing manual intervention and accelerating offer letter generation by 80%.",
+      "Designed and developed a comprehensive payment system within the LMS, enabling automated installment scheduling and tailored payment plans for both agents and students, while ensuring accurate logging of all payment records and significantly enhancing operational efficiency.",
+      "Spearheaded the integration of soft delete feature and update logs into existing systems, streamlining data recovery processes and enabling seamless state restoration, resulting in a 99% decrease in data loss incidents and improved system reliability.",
+      "Led frontend design and development for a renowned LMS client, making key architectural decisions, resolving complex issues, and building a robust coding framework.",
+    ],
   },
   {
-    title: "Frontend developer intern",
-    company: "Chop-chop, remote",
-    description:
-      "During my internship at Chop-Chop, I collaborated with an experienced IT team, focusing on tasks in Next.js. This experience provided valuable insights into teamwork and advanced my proficiency in Next.js.",
-    period: "2024",
-    technologies: ["Next.js", "React", "TypeScript", "Tailwind"],
+    institute: "SoftspaceAB",
+    position: "Junior Software Engineer",
+    time: "June 2020 - September 2021",
+    points: [
+      "Analyzed software requirements and client needs, researched modern solutions, and actively contributed to developer discussions while adhering to Agile principles and best practices to streamline development and reduce feature turnaround time.",
+      "Collaborated as part of a development team and took full ownership of the frontend design, creating an engaging and user-friendly interface for a social networking platform with Facebook-like features.",
+      "Designed and implemented a two-step verification authentication system along with a custom role-based access control (RBAC) mechanism to enhance security and enforce user-specific permissions.",
+      "Troubleshoot production escalations by deep diving into source code, analyzing network traces, and reviewing product configurations which helped to boost performance by 15%.",
+    ],
+  },
+  {
+    institute: "Khulna University",
+    position: "BSc & MSc in Computer Science & Engineering",
+    time: "January 2016 - September 2024",
+    points: [
+      "Secured 3rd positions in programming contest in KU CSE Fest-2017.",
+      "Participated in various onsite inter-university programming contests like ACM-ICPC, NCPC.",
+      "Solved around 400 problems in various online judges.",
+      "Convenor of KU CSE Fest 2019 & CSE Tour 2019.",
+      "Member of the finance committee in KU Rag Day-2019 & Bangla New Year Festival-2018.",
+      "Player of the department Volleyball & Football team.",
+    ],
   },
 ];
 
-export default EXPERIENCES;
+export default experiences;
